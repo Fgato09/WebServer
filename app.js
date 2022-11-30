ea02887abd3e2f4af7e045414dadff6c182504b3
Fgato09 <116131297+Fgato09@users.noreply.github.com>
2022-11-29 19:00:59 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var usuario_ruta = require('./rutas/usuarioRuta.js');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.use('/api', usuario_ruta);
+
+
+module.exports = app;
