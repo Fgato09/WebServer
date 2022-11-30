821658b786309c7cc9ed1bbdb7ef8b8f0a789a75
Fgato09 <116131297+Fgato09@users.noreply.github.com>
2022-11-29 23:25:38 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var app = require('./app');
 var port = process.env.PORT || 3877;
 
-mongoose.connect('mongodb://127.0.0.1:27017/ejemplo', (err, res) => {
+mongoose.connect('mongodb://127.0.0.1:27017/Practica3', (err, res) => {
     if (err) {
         throw err;
     } else {
